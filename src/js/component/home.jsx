229a03f85f70b31eb72c660f1b948ee928020139
Jaqa97 <joaquinonesa@gmail.com>
2022-12-29 02:10:56 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,6 +6,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [tareas, nueva] = useState(["Tarea 1", "Tarea 2", "Tarea 3"]);
+	const borrar = (b) => {
+		nueva(tareas.filter((value, index, arr) =>{
+			return index !=b
+		}))
+	}
 	return (
 		<div className="text-center">
 			<h1>To Do List!</h1>
@@ -16,7 +21,7 @@ const Home = () => {
 				<input class="my-2" placeholder="Agregar tarea"></input>
 			</form>
 			{tareas.map((value, index)=>{
-				return <li key={index} > {value} <button><i class="fas fa-trash"></i></button></li>			
+				return <li key={index} > {value} <button onClick={()=> borrar(index)}><i class="fas fa-trash"></i></button></li>			
 			})}
 			<form> </form>
 			
