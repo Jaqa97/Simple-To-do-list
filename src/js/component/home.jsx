ff8187359b7ffd96a1e6b966457bb6a4979abca5
Jaqa97 <joaquinonesa@gmail.com>
2022-12-29 01:54:05 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, nueva] = useState(["Tarea 1", "Tarea 2", "Tarea 3"]);
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>To Do List!</h1>
+			<form onSubmit={(event)=>{
+				event.preventDefault();
+				nueva([...tareas, event.target[0].value])
+			}}>
+				<input class="my-2" placeholder="Agregar tarea"></input>
+			</form>
+			{tareas.map((value, index)=>{
+				return <li key={index} > {value} <button><i class="fas fa-trash"></i></button></li>			
+			})}
+			<form> </form>
+			
 		</div>
 	);
 };
